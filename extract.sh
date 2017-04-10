dc0ecf5cc7605955c570b0e50a5d2548f8bcb414
Felipe de Macêdo Rodrigues <felipe.de.rodrigues@accenture.com>
2017-04-10 13:59:41 -0300

    init

---
+++ extract.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get install unzip
+
+unzip instantclient-basic-linux.x64-12.2.0.1.0.zip
+unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip
+unzip instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
+pwd
+ls
+echo "OK"
