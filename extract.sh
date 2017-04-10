331dc284bb755a77319fca03da9ba9fbc04669bb
Felipe de Macêdo Rodrigues <felipe.de.rodrigues@accenture.com>
2017-04-10 14:31:33 -0300

    house keeping after unzip

--- extract.sh
+++ extract.sh
@@ -5,6 +5,12 @@ apt-get install -y unzip
 unzip instantclient-basic-linux.x64-12.2.0.1.0.zip
 unzip instantclient-sdk-linux.x64-12.2.0.1.0.zip
 unzip instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
+
+rm instantclient-basic-linux.x64-12.2.0.1.0.zip
+rm instantclient-sdk-linux.x64-12.2.0.1.0.zip
+rm instantclient-sqlplus-linux.x64-12.2.0.1.0.zip
+rm -rf .git
+
 pwd
 ls
 echo "OK"
